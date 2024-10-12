89d88073e39c46cbdb11eeaef9d23d62e3bd40d7
= <=>
2024-10-12 13:17:27 -0600

    Refactor to use props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Post from "./components/Post";
 
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post author="Tommy" body="React.js is awesome!" />
+      <Post author="Max" body="check out the tutorial" />
+      <Post />
+      <Post />
+    </main>
+  );
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,8 @@
-const names = ["Tommy", "James"];
-
-function Post() {
-  // 50-50 chance of choosing a name from our array
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
+function Post(props) {
   return (
     <div>
-      <p>{chosenName}</p>
-      <p>React.js is awesome!</p>
+      <p>{props.author}</p>
+      <p>{props.body}</p>
     </div>
   );
 }
