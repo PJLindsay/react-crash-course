d3d90a042ef1073ce052bd8c4fe6c2123ae1ce6b
= <=>
2024-10-12 13:46:00 -0600

    Add NewPost component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Tommy" body="React.js is awesome!" />
-      <Post author="Max" body="check out the full course!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Tommy" body="React.js is awesome!" />
+        <Post author="Max" body="check out the full course!" />
+      </ul>
+    </>
   );
 }
 
