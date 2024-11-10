db0b9937751dc3a5c329770c34499c93bf0cc496
PJ Lindsay <pjlindsay@gmail.com>
2024-11-10 16:30:42 -0700

    Output list data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -26,9 +26,20 @@ function PostsList({ isPosting, onStopPosting }) {
     <>
       {modalContent}
 
-      <ul className={classes.posts}>
-        <Post author="Max" body="check out the full course!" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>No posts yet</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
